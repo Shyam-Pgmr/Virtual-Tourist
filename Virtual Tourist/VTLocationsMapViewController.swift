69e2339fba15c0b065e2d0c7b11b8e5c4b0364f5
Shyam <shyam.pgmr@gmail.com>
2017-06-19 18:19:03 +0530

    Feat: Setup MapViewController

---
+++ Virtual Tourist/VTLocationsMapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  VTLocationsMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Shyam on 19/06/17.
+//  Copyright © 2017 Shyam. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class VTLocationsMapViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var mapView:MKMapView!
+    
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
