72ad366a4886e73bfa1cae6bcac26efa61589f43
Shyam <shyam.pgmr@gmail.com>
2017-06-20 11:50:02 +0530

    Feat: On long pressing MapView, add PinAnnotation on that Location

--- Virtual Tourist/VTLocationsMapViewController.swift
+++ Virtual Tourist/VTLocationsMapViewController.swift
@@ -20,4 +20,49 @@ class VTLocationsMapViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    // MARK: Action
+    
+    @IBAction func longPressGestureOnMapView(_ sender: UILongPressGestureRecognizer) {
+        
+        if sender.state == .began {
+            
+            let touchPoint = sender.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            
+            addAnnotation(at: newCoordinates)
+        }
+    }
+    
+    // MARK: Helpers
+    
+    func addAnnotation(at coordinates:CLLocationCoordinate2D) {
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        mapView.addAnnotation(annotation)
+    }
+
+}
+
+extension VTLocationsMapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("tapped")
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "PinAnnotation")
+        pinAnnotation.pinTintColor = .red;
+        pinAnnotation.animatesDrop = true;
+        pinAnnotation.canShowCallout = false;
+        pinAnnotation.setSelected(true, animated: true)
+        
+        return pinAnnotation
+    }
+
 }
